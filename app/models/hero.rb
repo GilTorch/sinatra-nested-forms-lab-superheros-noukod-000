d0923e966c1a894c7215bb4aad78ac31cf4d2cbc
Gilbert Torchon <gilbert@noukod.com>
2019-01-10 17:33:29 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,13 +1,16 @@
 class Hero
   attr_accessor :name,:power,:biography
+
+  @@all=[]
+
   def initialize(name,power,biography)
     @name=name
     @power=power
     @biography=biography
   end
 
-  def self.all 
+  def self.all
     @@all
   end
-  
+
 end
