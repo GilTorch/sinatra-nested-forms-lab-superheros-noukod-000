5e9573f83675a8cf060c18cda5fcb22ed309e8a5
Gilbert Torchon <gilbert@noukod.com>
2019-01-10 17:47:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class App < Sinatra::Base
       @params["team"]["members"].each do |hero|
         Hero.new(hero.name,hero.power,hero.biography)
       end
+      @heroes=Hero.all
       erb :team
     end
 
