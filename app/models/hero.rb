8738e32fae639568700d25cde203fd32475312fd
Gilbert Torchon <gilbert@noukod.com>
2019-01-10 17:33:20 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,8 +1,13 @@
-class Hero 
+class Hero
   attr_accessor :name,:power,:biography
   def initialize(name,power,biography)
     @name=name
     @power=power
     @biography=biography
   end
+
+  def self.all 
+    @@all
+  end
+  
 end
